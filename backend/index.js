22739429d41344d34c98528303b4ae0c20b5bf23
RohitRao129 <rohityada128@gmail.com>
2023-09-13 18:51:33 +0530

    same as previous message

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,15 @@ const app =express();
 app.use(express.json());
 dbConnect();
 
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+})
+
 app.get('/', (req,res)=>{
     res.send('this is Home');
 })

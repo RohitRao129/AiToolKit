ffc051f8d6739b44f7e23c8b0611f8f24d9aea84
RohitRao129 <rohityada128@gmail.com>
2023-09-14 16:19:11 +0530

    v-0.1

--- backend/routes/Login.js
+++ backend/routes/Login.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 const Account = require("../models/Accounts");
 const {body, validationResult} = require('express-validator');
+const bcrypt = require("bcryptjs");
 
 router.post("/login", 
-[
-    body("username").isLength({min:5,max:24}),
-    body('password').isLength({min:6,max:24})
-],
+    //---------Express validator--------------------
+    [
+        body("username").isLength({min:5,max:24}),
+        body('password').isLength({min:6,max:24})
+    ],
 
 async (req,res)=>{
 
@@ -17,15 +19,22 @@ async (req,res)=>{
         return res.status(400).json({success:false,errors: validateerror.array()});
     }
 
-    let username = req.body.username;
+
+    
     try{
+
+        let username = req.body.username;
         let searchresult = await Account.findOne({username});
+        
+        console.log(searchresult);
 
         if(!searchresult){
             return res.status(400).json({success:false,errors: "User not found!"});
         }
 
-        if(req.body.password!==searchresult.password){
+        const passMatchResulet = await bcrypt.compare(req.body.password, searchresult.password);
+
+        if(!passMatchResulet){
             return res.status(400).json({success:false,errors: "Incorrect password!"});
         }
 
--- backend/routes/SignUp.js
+++ backend/routes/SignUp.js
@@ -2,14 +2,16 @@ const express = require("express");
 const router = express.Router();
 const Account = require("../models/Accounts");
 const {body, validationResult} = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 
 router.post("/signup", 
+    //-----express validator------------
     [
         body("username").isLength({min: 5, max: 24}),
         body("email").isEmail(),
-        body('password').isLength({min:8,max:24})
-        //body('confirmpassword').
+        body('password').isLength({min:8,max:24}),
+        body('confirmpassword').custom((value, {req}) => value === req.body.password).withMessage("The passwords do not match"),
     ], 
     
     async (req, res) => {
@@ -20,10 +22,29 @@ router.post("/signup",
         if (! validateerror.isEmpty()) {
             return res.status(400).json({errors: validateerror.array()});
         }
-       
-        
 
-        await Account.create({username: req.body.username, name: req.body.name, email: req.body.email, password: req.body.password})
+        //-------------checking if creds are awilable-------------
+
+        let username = req.body.username;
+        let email = req.body.email;
+
+        let searchresult = await Account.findOne({username});
+        if(searchresult){
+            return res.status(400).json({success:false,errors: "Username not awailable"});
+        }
+
+        searchresult = await Account.findOne({email});
+        if(searchresult){
+            return res.status(400).json({success:false,errors: "Email is already being used"});
+        }
+
+        //----------Hashing the password using bcryptjs-------------------------------------
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password,salt); 
+
+        //---------making new account entry in database--------------------------
+        await Account.create({username: req.body.username, name: req.body.name, email: req.body.email, password: hashedPassword})
 
         res.json({success: true, message: "New account created successfully."})
 
